b0617bcbc2e14e476db971d0261a997f4cf13dbb
eeerrrttty <romariocarnero@hotmail.com>
2019-04-26 11:01:18 -0400

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  */
 
 var Canvas = require('canvas');
-var Image = Image = Canvas.Image;
+var Image = Image = new Canvas.Image();
 var fs = require('fs');
 
 exports.scanFile = function(path, cb) {
@@ -30,7 +30,7 @@ exports.scanData = function(data, cbPrimary) {
 		ctx = canvas.getContext('2d');
 	},
 	loadImage = function(){
-		var img = new Image();
+		var img = new Image;
 		img.src = data;
 		// apply the width and height to the canvas element
 		canvas.width = img.width;
